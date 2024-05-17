4b298dfc7de887a42d609452b836bab5a1385bc0
daniel <armando.daniel.martinez.gonzalez@gmail.com>
2024-05-17 18:57:13 +0000

    mmigraciones

--- nasa/settings.py
+++ nasa/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'nasa_bd',
         'USER': 'postgres',
         'PASSWORD': 'armand99',
-        'HOST': '34.72.82.141',
+        'HOST': '35.227.167.96',
         'PORT': '5432'
     }
 }
